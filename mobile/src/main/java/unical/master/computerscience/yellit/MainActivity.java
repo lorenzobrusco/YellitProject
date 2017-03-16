1dadefdb0c541a5e09edd44d8e547e5255d81dbd
lorenzobrusco <lollobrusco@hotmail.it>
2017-03-16 10:23:10 +0100

    add some features

--- mobile/src/main/java/unical/master/computerscience/yellit/MainActivity.java
+++ mobile/src/main/java/unical/master/computerscience/yellit/MainActivity.java
@@ -9,16 +9,11 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.Toast;
-
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation;
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import unical.master.computerscience.yellit.graphic.Fragments.PostFragment;
-import unical.master.computerscience.yellit.graphic.dialog.CustomDialogBottomSheet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private static int HOME = 2;
     private static int ADD = 3;
     private static int MENU = 4;
+
+    private int currentItem;
 
     @Bind(R.id.bottom_navigation_view)
     AHBottomNavigation mBottomNavigation;
@@ -41,47 +38,67 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-        this.mBottomSheetBehavior =  BottomSheetBehavior.from(bottomSheet);
-        this.mBottomSheetBehavior.setHideable(true);
-        this.mBottomSheetBehavior.setPeekHeight(300);
-        this.mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
         currentFragment = new PostFragment();
         MainActivity.this.setFragment(currentFragment);
-        this.setupBottomNavigation(mBottomNavigation);
+        this.setupViews();
+
     }
 
     @TargetApi(Build.VERSION_CODES.M)
-    private void setupBottomNavigation(final AHBottomNavigation mBottomNavigation) {
+    private void setupViews() {
         AHBottomNavigationItem itemFitness = new AHBottomNavigationItem(R.string.tab_fitness, R.drawable.ic_fitness_center_black_24, R.color.color_bottom_navigation);
         AHBottomNavigationItem itemProfile = new AHBottomNavigationItem(R.string.tab_profile, R.drawable.ic_person_black_24, R.color.color_bottom_navigation);
         AHBottomNavigationItem itemHome = new AHBottomNavigationItem(R.string.tab_home, R.drawable.ic_home_black_24dp, R.color.color_bottom_navigation);
         AHBottomNavigationItem itemAdd = new AHBottomNavigationItem(R.string.tab_add_post, R.drawable.ic_public_black_24, R.color.color_bottom_navigation);
         AHBottomNavigationItem itemSomeThing = new AHBottomNavigationItem(R.string.tab_something, R.drawable.ic_menu_black_24
                 , R.color.color_bottom_navigation);
-
-
         mBottomNavigation.addItem(itemFitness);
         mBottomNavigation.addItem(itemProfile);
         mBottomNavigation.addItem(itemHome);
         mBottomNavigation.addItem(itemAdd);
         mBottomNavigation.addItem(itemSomeThing);
-
         mBottomNavigation.setColoredModeColors(getResources().getColor(R.color.active_button_bottom_navitagiont, getTheme()), getResources().getColor(R.color.inactive_button_bottom_navitagiont, getTheme()));
         mBottomNavigation.setForceTint(true);
-
         mBottomNavigation.setTranslucentNavigationEnabled(true);
-
         mBottomNavigation.setTitleState(AHBottomNavigation.TitleState.SHOW_WHEN_ACTIVE);
+        this.currentItem = 2;
+        mBottomNavigation.setCurrentItem(currentItem);
 
-        mBottomNavigation.setCurrentItem(2);
-
+        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        mBottomSheetBehavior.setHideable(true);
+        mBottomSheetBehavior.setPeekHeight(300);
+        mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
         mBottomNavigation.setOnTabSelectedListener(new AHBottomNavigation.OnTabSelectedListener() {
             @Override
             public boolean onTabSelected(int position, boolean wasSelected) {
                 switch (position) {
+                    case 0:
+                        if (mBottomSheetBehavior.getState() != BottomSheetBehavior.STATE_HIDDEN) {
+                            mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                        }
+                        currentItem = 0;
+                        break;
+                    case 1:
+                        if (mBottomSheetBehavior.getState() != BottomSheetBehavior.STATE_HIDDEN) {
+                            mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                        }
+                        currentItem = 1;
+                        break;
+                    case 2:
+                        if (mBottomSheetBehavior.getState() != BottomSheetBehavior.STATE_HIDDEN) {
+                            mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                        }
+                        currentItem = 2;
+                        break;
+                    case 3:
+                        if (mBottomSheetBehavior.getState() != BottomSheetBehavior.STATE_HIDDEN) {
+                            mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                        }
+                        currentItem = 3;
+                        break;
                     case 4:
-                        if(mBottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
-                            mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                        if (mBottomSheetBehavior.getState() == BottomSheetBehavior.STATE_HIDDEN) {
+                            mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
                         } else {
                             mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
                         }
@@ -89,6 +106,24 @@ public class MainActivity extends AppCompatActivity {
                 }
 
                 return true;
+            }
+        });
+
+        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+
+                } else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+
+                } else if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                    if(mBottomNavigation.getCurrentItem() == 4)
+                        mBottomNavigation.setCurrentItem(currentItem);
+                }
+            }
+
+            @Override
+            public void onSlide(View bottomSheet, float slideOffset) {
             }
         });
 
