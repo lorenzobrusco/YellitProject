ce793bf691350b40b2c1fac58bbecd73f75468ac
lorenzobrusco <lollobrusco@hotmail.it>
2017-03-18 18:39:49 +0100

    add load page

--- mobile/src/main/java/unical/master/computerscience/yellit/graphic/Activities/LoginActivity.java
+++ mobile/src/main/java/unical/master/computerscience/yellit/graphic/Activities/LoginActivity.java
@@ -48,11 +48,6 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        /*requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-*/
         getSupportActionBar().hide();
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
