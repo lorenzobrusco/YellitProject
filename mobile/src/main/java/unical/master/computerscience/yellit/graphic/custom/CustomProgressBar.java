94b7b32d1ea963372afe7ec55fb1eb2d362016da
lorenzobrusco <lollobrusco@hotmail.it>
2017-04-03 11:25:24 +0200

    modify horizontal bar

---
+++ mobile/src/main/java/unical/master/computerscience/yellit/graphic/custom/CustomProgressBar.java
@@ -0,0 +1,26 @@
+package unical.master.computerscience.yellit.graphic.custom;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.daimajia.numberprogressbar.NumberProgressBar;
+
+/**
+ * Created by Lorenzo on 03/04/2017.
+ */
+
+public class CustomProgressBar extends NumberProgressBar {
+    public CustomProgressBar(Context context) {
+        super(context);
+    }
+
+    public CustomProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomProgressBar(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+}
