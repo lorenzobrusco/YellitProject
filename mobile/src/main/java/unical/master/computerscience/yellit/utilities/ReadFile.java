59291952a0d0b10bb4be73527bb583f186c9fe59
fracos23 <fracos23@gmail.com>
2017-04-24 12:13:09 +0200

    forget files

---
+++ mobile/src/main/java/unical/master/computerscience/yellit/utilities/ReadFile.java
@@ -0,0 +1,54 @@
+package unical.master.computerscience.yellit.utilities;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Created by Francesco on 04/04/2017.
+ */
+
+public class ReadFile {
+
+    private static ReadFile mReadFile;
+    private static final String EMPTY = "empty";
+
+    private ReadFile() {
+
+    }
+
+    public static ReadFile getInstance() {
+        if (mReadFile == null) {
+            mReadFile = new ReadFile();
+        }
+
+        return mReadFile;
+    }
+
+    public String readXMLFile(final Context context, String name) {
+        String filename = name + ".xml";
+        try {
+            FileInputStream fis = context.openFileInput(filename);
+            InputStreamReader isr = new InputStreamReader(fis, "UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(isr);
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            return sb.toString();
+        } catch (FileNotFoundException e) {
+            return EMPTY;
+        } catch (UnsupportedEncodingException e) {
+            return EMPTY;
+        } catch (IOException e) {
+            return EMPTY;
+        }
+    }
+
+}
