6c15338a86e0ad2cee656bd7f1ee0fe80022e6dc
SalvaIsa <salvaisa7@libero.it>
2017-05-17 16:05:51 +0200

    Weather Starting

---
+++ mobile/src/main/java/unical/master/computerscience/yellit/utilities/RemoteFetch.java
@@ -0,0 +1,90 @@
+package unical.master.computerscience.yellit.utilities;
+
+/**
+ * Created by Altair07 on 17/05/2017.
+ */
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Locale;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.os.Debug;
+import android.util.Log;
+
+import unical.master.computerscience.yellit.R;
+
+public class RemoteFetch {
+
+    private static final String OPEN_WEATHER_MAP_API =
+            "http://api.openweathermap.org/data/2.5/weather?q=%s&units=metric";
+
+    public static JSONObject getJSON(Context context, String city){
+        try {
+            URL url = new URL(String.format(OPEN_WEATHER_MAP_API, city));
+            HttpURLConnection connection =
+                    (HttpURLConnection)url.openConnection();
+
+            connection.addRequestProperty("x-api-key",
+                    context.getString(R.string.open_weather_maps_app_id));
+
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(connection.getInputStream()));
+
+            StringBuffer json = new StringBuffer(1024);
+            String tmp="";
+            while((tmp=reader.readLine())!=null)
+                json.append(tmp).append("\n");
+            reader.close();
+
+            JSONObject data = new JSONObject(json.toString());
+
+            // This value will be 404 if the request was not
+            // successful
+            if(data.getInt("cod") != 200){
+                return null;
+            }
+
+            return data;
+        }catch(Exception e){
+            return null;
+        }
+    }
+
+    public static void renderWeather(JSONObject json){
+        try {
+
+            json.getString("name").toUpperCase(Locale.US);
+            json.getJSONObject("sys").getString("country");
+
+            JSONObject details = json.getJSONArray("weather").getJSONObject(0);
+            JSONObject main = json.getJSONObject("main");
+
+            details.getString("description").toUpperCase(Locale.US);
+
+            Log.e("varie",
+                    details.getString("description").toUpperCase(Locale.US) +
+                            "\n" + "Humidity: " + main.getString("humidity") + "%" +
+                            "\n" + "Pressure: " + main.getString("pressure") + " hPa");
+
+            Log.e("TEMPERATURA",
+                    String.format("%.2f", main.getDouble("temp"))+ " ℃");
+
+            /*
+            DateFormat df = DateFormat.getDateTimeInstance();
+            String updatedOn = df.format(new Date(json.getLong("dt")*1000));
+            updatedField.setText("Last update: " + updatedOn);
+
+            setWeatherIcon(details.getInt("id"),
+                    json.getJSONObject("sys").getLong("sunrise") * 1000,
+                    json.getJSONObject("sys").getLong("sunset") * 1000);*/
+
+        }catch(Exception e){
+            Log.e("SimpleWeather", "One or more fields not found in the JSON data");
+        }
+    }
+}
